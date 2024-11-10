64369da1f13dfee708f51a4d39645fdc938e1cd6
jisuseo <heyho0929@gmail.com>
2024-11-10 23:56:50 +0100

    생코 4/7 INSERT

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -7,9 +7,17 @@ CREATE TABLE 'New_Table' (
     author_name TEXT NOT NULL,
     author_profile TEXT
     );
-    
+
+DROP TABLE New_Table
+
 INSERT INTO 'New_Table' VALUES
 (1, 'SQLite', 'SQLite is', '2022.03.22', 'egoing', 'programmer');
 
-INSERT INTO 'New_Table' VALUES
-(2, 'SQL
+INSERT INTO 'New_Table' (title, body, created, author_name, author_profile) 
+VALUES(
+    'SQL Server', 
+    'Sql Server is ...',
+    '2020.01.03',
+    'duru',
+    'DBA'
+    );
